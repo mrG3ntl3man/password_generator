7e2d77213b76a08c72c178f00f8b3d8e62f7d811
root <root@localhost.localdomain>
2022-02-10 09:58:20 +0100

    v.0.2

--- password_generator.py
+++ password_generator.py
@@ -8,20 +8,47 @@ punctuation = string.punctuation
 
 characters = list(lower_case + upper_case + digits + punctuation)
 
+print('')
+print('---------- PASSWORD GENERATOR ----------')
+print('')
+
 def generate_password():
-    print()
-    password_length = int(input('Podaj długość hasła, które chcesz wygenerować: '))
-    print()
-    random.shuffle(characters)
-    password = []
+    option = -1
 
-    for i in range(password_length):
-        password.append(random.choice(characters))
+    while option != '0':
+        print('Wybierz opcję: ')
+        print('0 - Wyjście z programu')
+        print('1 - Generuj nowe hasło')
+        print()
+
+        option = input('Wybieram opcję (0 lub 1): ')
+
+        if option == '1':
+            print()
+            password_length = int(input('Podaj długość hasła, które chcesz wygenerować: '))
+            print()
+
+            if password_length < 12:
+                print('Twoje hasło powinno składać się minimalnie z 12 znaków!')
+                print()
+                break
+            else:
+                print('Generuję hasło...')
+
+            random.shuffle(characters)
+            password = []
+
+            for i in range(password_length):
+                password.append(random.choice(characters))
     
-    random.shuffle(password)
-    password = ''.join(password)
+            random.shuffle(password)
+            password = ''.join(password)
 
-    print('Wygenerowane hasło: {}'.format(password))
-    print()
+            print('Wygenerowane hasło: {}'.format(password))
+            print()
+        elif option == '0':
+            print()
+            print('---------- DO ZOBACZENIA! ----------')
+            print()
 
 generate_password()
